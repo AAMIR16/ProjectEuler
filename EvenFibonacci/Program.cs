2a2bd11de44809504ff605c58f7723ad92c0ce07
AAMIR16 <aamir.abbas16@gmail.com>
2022-03-17 17:13:00 -0700

    Even fibonacci

---
+++ EvenFibonacci/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EvenFibonacci
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int n = 100;
+            int regcount = 0;long evencount = 0;
+            List<long> x = new List<long>();
+
+            for (int i = 0; i < n; i++)
+            {
+                if (i == 0)
+                    x.Add(1);
+                else if (i == 1)
+                    x.Add(1);
+                else
+                    x.Add(x[i-1]+x[i-2]);
+            }
+
+            foreach(var a in x)
+            {
+                if (a % 2 == 0)
+                    evencount += a;
+            }
+            
+            Console.WriteLine(evencount);
+
+            foreach(var b in x)            
+                Console.WriteLine(b);            
+        }
+    }
+}
